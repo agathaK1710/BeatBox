67dce715b28bc317101c231a713046330885e39b
Agatha <agatha.kazak@gmail.com>
2021-09-14 13:35:52 +0300

    Add BaseObservable

--- app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -1,10 +1,15 @@
 package com.bignerdranch.android.beatbox
 
-class SoundViewModel {
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel: BaseObservable() {
     var sound: Sound? = null
     set(sound) {
         field = sound
+        notifyChange()
     }
+    @get:Bindable
     val title: String?
     get() = sound?.name
 }
