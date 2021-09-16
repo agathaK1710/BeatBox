91ce22f95fe087cf81a8e1f0106132592a47e063
Agatha <agatha.kazak@gmail.com>
2021-09-16 09:42:42 +0300

    Test callsBeatPlayOnButtonClisked

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -4,21 +4,31 @@ import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 class SoundViewModelTest {
 
+    private lateinit var beatBox: BeatBox
     private lateinit var sound: Sound
     private lateinit var subject: SoundViewModel
 
     @Before
     fun setUp() {
+        beatBox = mock(BeatBox::class.java)
         sound = Sound("assetPath")
-        subject = SoundViewModel()
+        subject = SoundViewModel(beatBox)
         subject.sound = sound
     }
 
     @Test
     fun exposesSoundNamesAsTitle() {
         assertThat(subject.title, `is`(sound.name))
+    }
+
+    @Test
+    fun callsBeatPlayOnButtonClisked() {
+        subject.onButtonClicked()
+        verify(beatBox).play(sound)
     }
 }
