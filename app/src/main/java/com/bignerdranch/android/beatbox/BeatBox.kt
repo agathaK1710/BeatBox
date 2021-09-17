e85fe4ed285902da857e4a17123ffab4f192c911
Agatha <agatha.kazak@gmail.com>
2021-09-17 09:56:41 +0300

    feat: part 20

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,17 +1,35 @@
 package com.bignerdranch.android.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
     val sounds: List<Sound>
 
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
+
     init {
         sounds = loadSounds()
+    }
+
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
+    }
+
+    fun release() {
+        soundPool.release()
     }
 
     private fun loadSounds(): List<Sound> {
@@ -26,8 +44,19 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach {
             val assetPath = "$SOUNDS_FOLDER/$it"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException) {
+                Log.e(TAG, "Cound not load sound $it", ioe)
+            }
         }
         return sounds
+    }
+
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
     }
 }
